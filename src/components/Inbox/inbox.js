32ec7c2e83e682c660a6ba1913dd3e125a8372d4
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-05-10 09:27:22 +0530

    Mail Box optimisations-custom Hooks

--- src/components/Inbox/inbox.js
+++ src/components/Inbox/inbox.js
@@ -4,81 +4,93 @@ import AuthContext from "../../Store/auth-context";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import useFetchData from "../../Store-Redux/FetchData";
 
 const Inbox = (props) => {
   const [inbox, setInbox] = useState([]);
   const { inboxdata } = useContext(AuthContext);
-  const navigate=useNavigate()
-  const emailID=localStorage.getItem('email')
+  const navigate = useNavigate();
+  const emailID = localStorage.getItem("email");
   let cleanEmail = emailID.replace(/[@.]/g, "");
 
-    const showHandler = (event) => {
-        fetch(
-          `https://mail-box-react-default-rtdb.firebaseio.com/MailBox/${cleanEmail}/ReceivedMail.json`,
-          {
-            method: "GET",
-            headers: { "Content-Type": "application/JSON" },
-          }
-        )
-          .then((res) => {
-            if (res.ok) {
-              return res.json();
-            } else {
-              return res.json().then((data) => {
-                let errorMessage = "Authentication failed";
-                throw new Error(errorMessage);
-              });
-            }
-          })
-          .then((data) => {
-            //console.log(data);
-            setInbox(data)
+  const [newdata] = useFetchData(
+    `https://mail-box-react-default-rtdb.firebaseio.com/MailBox/${cleanEmail}/ReceivedMail.json`
+  );
+  const showHandler = () => {
+    setInbox(newdata);
+
+    // fetch(
+    //   `https://mail-box-react-default-rtdb.firebaseio.com/MailBox/${cleanEmail}/ReceivedMail.json`,
+    //   {
+    //     method: "GET",
+    //     headers: { "Content-Type": "application/JSON" },
+    //   }
+    // )
+    //   .then((res) => {
+    //     if (res.ok) {
+    //       return res.json();
+    //     } else {
+    //       return res.json().then((data) => {
+    //         let errorMessage = "Authentication failed";
+    //         throw new Error(errorMessage);
+    //       });
+    //     }
+    //   })
+    //   .then((data) => {
+    //     //console.log(data);
+    //     setInbox(data)
+    //   });
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      showHandler();
+    }, 3000);
+    return () => clearInterval(interval);
+  });
+
+  const deleteHandler = (event) => {
+    event.preventDefault();
+
+    fetch(
+      `https://mail-box-react-default-rtdb.firebaseio.com/MailBox/${cleanEmail}/ReceivedMail/${event.target.id}.json`,
+      {
+        method: "DELETE",
+        headers: { "Content-Type": "application/JSON" },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Deletion failed";
+            throw new Error(errorMessage);
           });
-    };
-
-    useEffect(() => {
-      const interval = setInterval(() => {
-        showHandler();
-      }, 3000)
-      return () => clearInterval(interval)
-    }, );
-  
-  const deleteHandler=(event)=>{
-       event.preventDefault();
-
-        fetch(`https://mail-box-react-default-rtdb.firebaseio.com/MailBox/${cleanEmail}/ReceivedMail/${event.target.id}.json`,{
-            method:'DELETE',
-            headers:{'Content-Type':'application/JSON'}
-        }).then(res=>{
-            if(res.ok){
-                return (res.json())
-            }
-            else {
-                return res.json().then((data) => {
-                  let errorMessage = "Deletion failed";
-                  throw new Error(errorMessage);
-                });
-              }
-            })
-            .then((data) => {
-              console.log("email deleted successfully");
-            })
-            .catch((err) => {
-              alert(err.message);
-            });
-    }
+        }
+      })
+      .then((data) => {
+        console.log("email deleted successfully");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
 
   const InboxList = [];
   for (let key in inbox) {
     const id = key;
-    const { Subject, To, Description,READ } = inbox[key];
+    const { Subject, To, Description, READ } = inbox[key];
     InboxList.push(
       <ul key={id}>
         <li className={classes.list}>
-        {!READ && <button className={classes.notification}>New</button>}
+          {!READ && <button className={classes.notification}>New</button>}
           Subject:{Subject} To:{To} Description:{Description}
-          <button onClick={()=>navigate(`/details/${id}`)} id={id}>See Details</button>
-          <button onClick={deleteHandler} id={id}>Delete</button>
+          <button onClick={() => navigate(`/details/${id}`)} id={id}>
+            See Details
+          </button>
+          <button onClick={deleteHandler} id={id}>
+            Delete
+          </button>
         </li>
       </ul>
     );
